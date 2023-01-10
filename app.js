41ff65d416de9a5a0f9d3ee64763400aef7bb3bd
Jesse Carson <105603968+jcarson222@users.noreply.github.com>
2023-01-10 10:24:53 -0600

    Update app.js
    Co-authored-by: Justin Boltz <justin.boltz@sourceday.com>

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', function() {
 // show person based on item
 
 function showPerson(person) {
-  const item = reviews [person];
+  const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
